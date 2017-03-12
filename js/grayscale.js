07230d09a1c911350cc666342ef8c064c021a161
Sharmaine <hello@sharmaine.me>
2017-03-11 16:05:07 -0800

    added one liner

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,5 +1,5 @@
 /*!
- * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Start Bootstrap 
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
@@ -182,4 +182,29 @@ function init() {
         map: map,
         icon: image
     });
+
+    // Disable scroll zooming and bind back the click event
+    var onMapMouseleaveHandler = function (event) {
+      var that = $(this);
+
+      that.on('click', onMapClickHandler);
+      that.off('mouseleave', onMapMouseleaveHandler);
+      that.find('iframe').css("pointer-events", "none");
+    }
+
+    var onMapClickHandler = function (event) {
+      var that = $(this);
+
+      // Disable the click handler until the user leaves the map area
+      that.off('click', onMapClickHandler);
+
+      // Enable scrolling zoom
+      that.find('iframe').css("pointer-events", "auto");
+
+      // Handle the mouse leave event
+      that.on('mouseleave', onMapMouseleaveHandler);
+    }
+
+    // Enable map zooming with mouse scroll when the user clicks the map
+    $('.maps.embed-container').on('click', onMapClickHandler);
 }
